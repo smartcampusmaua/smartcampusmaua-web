aaca9f39590f3b6c036e2e4782a1c9877a5869f5
Murilo Molina Barone <murilo.m.barone@gmail.com>
2025-01-08 11:45:05 -0300

    Login page responsiveness

--- src/app/modulos/page.tsx
+++ src/app/modulos/page.tsx
@@ -41,30 +41,29 @@ export default function Page() {
   return (
       <>
         <Head>
-          <title>EcoVision</title>
+          <title>SmartCampus Mauá</title>
         </Head>
-        <div className="flex h-screen items-center justify-center bg-neutral-100">
-          <div className='rounded-xl bg-white p-8 w-1/2 animate-fade-in' style={{ boxShadow: '8px 8px 25px rgba(0,0,0,.2)' }}>
+        <div className="flex h-screen items-center justify-center bg-neutral-100 p-4"> 
+          <div className='rounded-xl bg-white p-4 sm:p-8 w-full sm:w-1/2 animate-fade-in' style={{ boxShadow: '8px 8px 25px rgba(0,0,0,.2)' }}>
             <div className="flex justify-between items-start">
-                <div>
-                    <h1 className='text-xl'>Seja bem-vindo, <span className='font-medium'>{displayName}</span>.</h1>
-                    <p className=' text-neutral-600'>Selecione o módulo que deseja acessar.</p>
-                </div>
-                <button onClick={handleMicrosoftLogout}>Sair</button>
+              <div>
+                <h1 className='text-base sm:text-xl'>Seja bem-vindo, <span className='font-medium'>{displayName}</span>.</h1>
+                <p className=' text-neutral-600'>Selecione o módulo que deseja acessar.</p>
+              </div>
+              <button onClick={handleMicrosoftLogout}>Sair</button>
             </div>
             <div className='grid mt-10 place-items-center text-center'>
-            {/* <a href={`${process.env.NEXT_PUBLIC_SMARTCAMPUSMAUA_WEB_URL}:${process.env.NEXT_PUBLIC_SMARTCAMPUSMAUA_WEB_PORT}/gms/devices`}> */}
-            <a href={`${SMARTCAMPUSMAUA_WEB}/gms/devices`}>
-            
-            <div className='w-72 p-6 hover:opacity-75 transition rounded-xl flex flex-col space-y-3 justify-center bg-gray-200' style={{ boxShadow: '8px 8px 25px rgba(0,0,0,.2)' }} >
-                <img className="object-contain h-20" src='/images/logo_maua.svg' alt="IMT" />
-
-                    <span className={`text-2xl text-gms-tertiary ${outfit.className} antialiased`}><span className="text-blue-900">SmartCampus Mauá</span> </span>
+              <a href={`${SMARTCAMPUSMAUA_WEB}/gms/devices`}>
+                <div className='w-full sm:w-72 p-6 hover:opacity-75 transition rounded-xl flex flex-col space-y-3 justify-center bg-gray-200' style={{ boxShadow: '8px 8px 25px rgba(0,0,0,.2)' }}>
+                  <img className="object-contain h-20" src='/images/logo_maua.svg' alt="IMT" />
+                  <span className={`text-base sm:text-2xl text-gms-tertiary ${outfit.className} antialiased`}>
+                    <span className="text-blue-900">SmartCampus Mauá</span>
+                  </span>
                 </div>
-                </a>
+              </a>
             </div>
           </div>
         </div>
       </>
-  );
+    );
 }
