62796624bf47b96949c44dcc5163b2c39be7772e
Vinicius Parelho <vinicius.oliveira@maua.br>
2025-01-10 09:55:47 -0300

    generic fetch sensors function

--- src/app/gms/devices/page.tsx
+++ src/app/gms/devices/page.tsx
@@ -3,272 +3,32 @@
 import Head from "next/head";
 import DashboardLayout from "@/app/gms/components/DashboardLayout";
 import { useState, useEffect } from "react";
-import { fetchAllSensors } from "@/database/timeseries";
-import {
-  // Luz, 
-  // Local, 
-  GenericSensor
-} from "@/database/dataTypes";
-import {
-  supabase,
-  // getData 
-} from '@/database/supabaseClient';
+import { GenericSensor } from "@/database/dataTypes";
+import { supabase } from '@/database/supabaseClient';
 import { formatDistanceToNow } from 'date-fns';
 import { pt } from 'date-fns/locale';
+import { fetchSensors } from '@/database/timeseries';
 
 const Sensores = () => {
-  const [sensors, setSensores] = useState<GenericSensor[]>([]);
+  const [sensors, setSensors] = useState<GenericSensor[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [filteredSensores, setFilteredSensores] = useState<GenericSensor[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
   const SMARTCAMPUSMAUA_SERVER = `${process.env.NEXT_PUBLIC_SMARTCAMPUSMAUA_SERVER_URL}:${process.env.NEXT_PUBLIC_SMARTCAMPUSMAUA_SERVER_PORT}`;
 
-  const sanitize = (value: any) => (value === "" || value === null ? "Indisponível" : value);
-
   useEffect(() => {
-    const fetchSensors = async () => {
-      const { data: sensorsInfo, error } = await supabase
-        .from('Sensors')
-        .select("Nome, DEVEUI, Local, Tipo");
-
-      if (error) {
-        console.error('Error fetching sensors data: ', error);
-        return;
-      }
-
-      const sensorsData = await fetchAllSensors();
-
-      setSensores(prevSensores => {
-        const updatedSensores = [...prevSensores];
-
-        sensorsData.forEach(sensorData => {
-          const isDuplicate = updatedSensores.some(
-            sensor => sanitize(sensor.tags[0]) === sanitize(sensorData.tags.deviceId)
-          );
-          var sensorAlreadyExists = false;
-
-          if (!isDuplicate) {
-            let newSensor;
-            if (sensorData.name === "SmartLight" && !sensorAlreadyExists) {
-              sensorsInfo.forEach(sensorInfo => {
-                if (sensorInfo.DEVEUI == sensorData.tags.deviceId) {
-                  newSensor = new GenericSensor(
-                    sanitize(sensorInfo.Nome),
-                    sensorData.name,
-                    [
-                      sanitize((Number(sensorData.fields.batteryVoltage) / 1000).toFixed(1)) + " V",
-                      sanitize(sensorData.fields.boardVoltage) + " V",
-                      sanitize(sensorData.fields.humidity) + " %",
-                      sanitize((Math.pow(Number(sensorData.fields.luminosity), -3.746) * 140000000000000).toFixed(1)) + " lux",
-                      sanitize(sensorData.fields.temperature) + " °C",
-                      sanitize(sensorData.fields.movement)
-                    ],
-                    [sanitize(sensorData.tags.deviceId)],
-                    sanitize(sensorInfo.Local),
-                    new Date(Number(sensorData.timestamp) / 1e6)
-                  );
-                  sensorAlreadyExists = true;
-                }
-              });
-              if (!sensorAlreadyExists) {
-                newSensor = new GenericSensor(
-                  "Indisponível",
-                  sensorData.name,
-                  [
-                    sanitize((Number(sensorData.fields.batteryVoltage) / 1000).toFixed(1)) + " V",
-                    sanitize(sensorData.fields.boardVoltage) + " V",
-                    sanitize(sensorData.fields.humidity) + " %",
-                    sanitize((Math.pow(Number(sensorData.fields.luminosity), -3.746) * 140000000000000).toFixed(1)) + " lux",
-                    sanitize(sensorData.fields.temperature) + " °C",
-                    sanitize(sensorData.fields.movement)
-                  ],
-                  [sanitize(sensorData.tags.deviceId)],
-                  "Indisponível",
-                  new Date(Number(sensorData.timestamp) / 1e6)
-                );
-              }
-            } else if (sensorData.name === "WaterTankLevel") {
-              sensorsInfo.forEach(sensorInfo => {
-                if (sensorInfo.DEVEUI == sensorData.tags.deviceId) {
-                  newSensor = new GenericSensor(
-                    sanitize(sensorInfo.Nome),
-                    sensorData.name,
-                    [
-                      sanitize((sensorData.fields.boardVoltage).toFixed(1)) + " V",
-                      sanitize((Number(sensorData.fields.distance) / 1000).toFixed(1)) + " metros"
-                    ],
-                    [sanitize(sensorData.tags.deviceId)],
-                    sanitize(sensorInfo.Local),
-                    new Date(Number(sensorData.timestamp) / 1e6)
-                  );
-                  sensorAlreadyExists = true;
-                }
-              });
-              if (!sensorAlreadyExists) {
-                newSensor = new GenericSensor(
-                  "Indisponível",
-                  sensorData.name,
-                  [
-                    sanitize((sensorData.fields.boardVoltage).toFixed(1)) + " V",
-                    sanitize((Number(sensorData.fields.distance) / 1000).toFixed(1)) + " metros"
-                  ],
-                  [sanitize(sensorData.tags.deviceId)],
-                  "Indisponível",
-                  new Date(Number(sensorData.timestamp) / 1e6)
-                );
-              }
-            } else if (sensorData.name === "Hydrometer") {
-              sensorsInfo.forEach(sensorInfo => {
-                if (sensorInfo.DEVEUI == sensorData.tags.deviceId) {
-                  newSensor = new GenericSensor(
-                    sanitize(sensorInfo.Nome),
-                    sensorData.name,
-                    [
-                      sanitize(sensorData.fields.boardVoltage).toFixed(1) + " V",
-                      sanitize(sensorData.fields.counter)
-                    ],
-                    [sanitize(sensorData.tags.deviceId)],
-                    sanitize(sensorInfo.Local),
-                    new Date(Number(sensorData.timestamp) / 1e6)
-                  );
-                  sensorAlreadyExists = true;
-                }
-              });
-              if (!sensorAlreadyExists) {
-                newSensor = new GenericSensor(
-                  "Indisponível",
-                  sensorData.name,
-                  [
-                    sanitize(sensorData.fields.boardVoltage).toFixed(1) + " V",
-                    sanitize(sensorData.fields.counter)
-                  ],
-                  [sanitize(sensorData.tags.deviceId)],
-                  "Indisponível",
-                  new Date(Number(sensorData.timestamp) / 1e6)
-                );
-              }
-            } else if (sensorData.name === "EnergyMeter") {
-              sensorsInfo.forEach(sensorInfo => {
-                if (sensorInfo.DEVEUI == sensorData.tags.deviceId) {
-                  newSensor = new GenericSensor(
-                    sanitize(sensorInfo.Nome),
-                    sensorData.name,
-                    [
-                      sanitize(sensorData.fields.boardVoltage).toFixed(1) + " V",
-                      sanitize(sensorData.fields.forwardEnergy).toFixed(1) + " kWh",
-                      sanitize(sensorData.fields.reverseEnergy).toFixed(1) + " kWh"
-                    ],
-                    [sanitize(sensorData.tags.deviceId)],
-                    sanitize(sensorInfo.Local),
-                    new Date(Number(sensorData.timestamp) / 1e6)
-                  );
-                  sensorAlreadyExists = true;
-                }
-              });
-              if (!sensorAlreadyExists) {
-                newSensor = new GenericSensor(
-                  "Indisponível",
-                  sensorData.name,
-                  [
-                    sanitize(sensorData.fields.boardVoltage).toFixed(1) + " V",
-                    sanitize(sensorData.fields.forwardEnergy).toFixed(1) + " kWh",
-                    sanitize(sensorData.fields.reverseEnergy).toFixed(1) + " kWh"
-                  ],
-                  [sanitize(sensorData.tags.deviceId)],
-                  "Indisponível",
-                  new Date(Number(sensorData.timestamp) / 1e6)
-                );
-              }
-            } else if (sensorData.name === "WeatherStation") {
-              sensorsInfo.forEach(sensorInfo => {
-                if (sensorInfo.DEVEUI == sensorData.tags.deviceId) {
-                  newSensor = new GenericSensor(
-                    sanitize(sensorInfo.Nome),
-                    sensorData.name,
-                    [
-                      sanitize(sensorData.fields.emwAtmPres),
-                      sanitize(sensorData.fields.emwAvgWindSpeed),
-                      sanitize(sensorData.fields.emwGustWindSpeed),
-                      sanitize(sensorData.fields.emwHumidity) + " %",
-                      sanitize(sensorData.fields.emwLuminosity),
-                      sanitize(sensorData.fields.emwRainLevel),
-                      sanitize(sensorData.fields.emwSolarRadiation),
-                      sanitize(sensorData.fields.emwTemperature) + " °C",
-                      sanitize(sensorData.fields.emwUv)
-                    ],
-                    [sanitize(sensorData.tags.deviceId)],
-                    sanitize(sensorInfo.Local),
-                    new Date(Number(sensorData.timestamp) / 1e6)
-                  );
-                  sensorAlreadyExists = true;
-                }
-              });
-              if (!sensorAlreadyExists) {
-                newSensor = new GenericSensor(
-                  "Indisponível",
-                  sensorData.name,
-                  [
-                    sanitize(sensorData.fields.emwAtmPres),
-                    sanitize(sensorData.fields.emwAvgWindSpeed),
-                    sanitize(sensorData.fields.emwGustWindSpeed),
-                    sanitize(sensorData.fields.emwHumidity) + " %",
-                    sanitize(sensorData.fields.emwLuminosity),
-                    sanitize(sensorData.fields.emwRainLevel),
-                    sanitize(sensorData.fields.emwSolarRadiation),
-                    sanitize(sensorData.fields.emwTemperature) + " °C",
-                    sanitize(sensorData.fields.emwUv)
-                  ],
-                  [sanitize(sensorData.tags.deviceId)],
-                  "Indisponível",
-                  new Date(Number(sensorData.timestamp) / 1e6)
-                );
-              }
-            }
-
-            if (newSensor) {
-              updatedSensores.push(newSensor);
-            }
-          }
-        });
-
-        sensorsInfo.forEach(sensorInfo => {
-          const isMissingInSensorsData = !sensorsData.some(sensorData =>
-            sanitize(sensorData.tags.deviceId) === sanitize(sensorInfo.DEVEUI)
-          );
-
-          if (isMissingInSensorsData) {
-            const alreadyExists = updatedSensores.some(sensor =>
-              sanitize(sensor.tags[0]) === sanitize(sensorInfo.DEVEUI)
-            );
-
-            if (!alreadyExists) {
-              updatedSensores.push(
-                new GenericSensor(
-                  sanitize(sensorInfo.Nome),
-                  sanitize(sensorInfo.Tipo),
-                  ["Sensor Offline"],
-                  [sanitize(sensorInfo.DEVEUI)],
-                  sanitize(sensorInfo.Local)
-                )
-              );
-            }
-          }
-        });
-
-        return updatedSensores;
-      });
-
+    async function getSensors() {
+      setSensors(await fetchSensors());
       setLoading(false);
-    };
+    }
 
-    fetchSensors();
+    getSensors();
   }, []);
-
 
   // Filtra os sensores com base no filtro e na busca
   useEffect(() => {
     const filtered = sensors.filter((sensor) => {
-      return Object.values(sensor).some((value) => 
+      return Object.values(sensor).some((value) =>
         value?.toString().toLowerCase().includes(searchQuery.toLowerCase())
       );
     });
@@ -312,9 +72,9 @@ const Sensores = () => {
               alreadyPlayed: false
             },
           ]);
-          if (!error) setAlarmInsertAttempt(false);
+        if (!error) setAlarmInsertAttempt(false);
       }
-    } 
+    }
 
     if (alarmInsertAttempt === true) setAlarmPopupOpen(false);
   }
