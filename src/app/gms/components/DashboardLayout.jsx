9af40b83e4abc0a13ae4398c74c2b35e81d247f3
Murilo Molina Barone <murilo.m.barone@gmail.com>
2025-01-14 14:55:56 -0300

    evse initial page

--- src/app/gms/components/DashboardLayout.jsx
+++ src/app/gms/components/DashboardLayout.jsx
@@ -122,6 +122,19 @@ const Layout = ({ children }) => {
                   </svg>
                   <span>SENSORES</span>
                 </a>
+                <a
+                  href="/gms/evse"
+                  className={`flex items-center space-x-2 text-nowrap rounded-full px-3 py-2.5 text-[0.825rem] tracking-wide transition  ${pathname === "/devices" ? "bg-blue-500 text-white" : "hover:bg-gray-200"
+                    }`}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                  <g>
+                      <path fill="none" d="M0 0h24v24H0z"/>
+                      <path d="M20 11h-1V7h1V4h2v3h1v4h-1v7a3 3 0 0 1-6 0v-4h-2v5h1v2H2v-2h1V4a1 1 0 0 1 1-1h9a1 1 0 0 1 1 1v8h2a2 2 0 0 1 2 2v4a1 1 0 0 0 2 0v-7zm-8 8V5H5v14h7zm-3-8h3l-4 6v-4H5l4-6v4z"/>
+                  </g>
+                  </svg>
+                  <span>EVSE</span>
+                </a>
               </div>
               <p className="pl-3 font-medium dark:text-white">Alarme</p>
               <div className="mt-4 flex flex-col space-y-3">
